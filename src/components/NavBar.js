0d86b7e80e2cfd1ec6997b759e60d780991a9f8e
Ahmed-Abdella <abdellaa921@gmail.com>
2022-01-11 03:46:39 +0200

    create a seacrhbar and show search results in search page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 export default function NavBar() {
   return (
@@ -8,6 +9,7 @@ export default function NavBar() {
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
+        <SearchBar />
         <Link to="/create"> Create Recipe</Link>
       </nav>
     </div>
