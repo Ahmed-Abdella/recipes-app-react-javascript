5521fbdf8c824f2ec5f7f0414f4c7ac1cc4cc08b
Ahmed-Abdella <abdellaa921@gmail.com>
2022-01-26 01:37:57 +0200

    creating a custom context hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,13 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
+import { useTheme } from "../hooks/useTheme";
+
 export default function NavBar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
